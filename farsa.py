2585ade181818fc8ddc8fda8d09e3ff792d4b915
ranaehab <ranae92.re@gmail.com>
2025-07-04 20:47:47 +0300

    Update farsa.py

--- farsa.py
+++ farsa.py
@@ -5,16 +5,12 @@ from farasa.segmenter import FarasaSegmenter
 from farasa.stemmer import FarasaStemmer
 import csv
 
-#source: https://r12a.github.io/scripts/tutorial/summaries/arabic
-sample =\
-'''
-دراسه دور انزيم التيلوميراز العكسي في تنظيم التعبير الجيني لبعض الجينات.
-'''
-f1 = open('D:/PHD Papers/scientific translation/Technical/Farsa/LastDataset/New Text Document.txt', 'r',encoding='utf8')
+
+f1 = open('SourceFile.txt', 'r',encoding='utf8')
 for line in f1:
     segmenter = FarasaSegmenter()
     segmented = segmenter.segment(line)
-    f2 = open('D:/PHD Papers/scientific translation/Technical/Farsa/LastDataset/TestFarsa.txt', 'a',encoding='utf8')
+    f2 = open('OutputFile.txt', 'a',encoding='utf8')
     f2.write(segmented)
     f2.write('\n')
     f2.close()
